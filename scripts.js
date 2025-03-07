1488ae5aa84b02ace1d285711509604e452d3d97
JavaAndWin <sliang528@gmail.com>
2025-03-07 23:12:46 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// scripts.js 文件
+
+// 获取按钮和消息元素
+const button = document.getElementById('showMessageButton');
+const message = document.getElementById('message');
+
+// 为按钮添加点击事件监听器
+button.addEventListener('click', () => {
+    // 显示隐藏的消息
+    message.style.display = 'block';
+    hideButton();
+});
+
+function hideButton() {
+    document.getElementById("showMessageButton").style.display = "none";
+}
